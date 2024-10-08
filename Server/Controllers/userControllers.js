cfcfb11018582a0f0b893ca28cdf77055dbd99d1
huynguyen12536 <huynguyen12536@gmail.com>
2024-10-08 12:54:35 +0700

    them phan tim nguoi dung+ bo ma hoa passs tren db

--- Server/Controllers/userControllers.js
+++ Server/Controllers/userControllers.js
@@ -1,8 +1,8 @@
 const userModel = require("../Models/userModel");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const validator = require("validator");
 
+// Hàm tạo token
 const createToken = (_id) => {
     const jwtkey = process.env.JWT_SECRET_KEY;
 
@@ -10,55 +10,52 @@ const createToken = (_id) => {
 };
 
 const registerUser = async (req, res) => {
-
     try {
         const { name, email, password, role, friends, groups } = req.body;
-
         let user = await userModel.findOne({ email });
-
         if (user) {
             return res.status(400).json("nguoi dung da ton tai");
         };
         if (!name || !email || !password) {
             return res.status(400).json("vui long nhap day du thong tin");
         };
-        if (!validator.isEmail(email)) return res.status(400).json("email khong kha dung 2");
-
-        if (!validator.isStrongPassword(password)) return res.status(400).json("mat khau ngan qua m!!!");
-
+        if (!validator.isEmail(email)) return res.status(400).json("email khong kha dung");
+        if (!validator.isStrongPassword(password)) return res.status(400).json("mat khau ngan qua!");
         user = new userModel({ name, email, password, role, friends, groups });
-
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-
         await user.save();
         const token = createToken(user._id);
-
         res.status(200).json({ _id: user._id, name, email, role, token });
     } catch (error) {
-        console.log9(error);
-
+        console.log(error);
         res.status(500).json("server error");
     }
-
 };
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
         let user = await userModel.findOne({ email });
-
         if (!user) return res.status(400).json("email khong dung");
-
-        const isValidPassword = await bcrypt.compare(password, user.password);
-
-        if (!isValidPassword) return res.status(400).json("mat khau khong dung");
+        if (password !== user.password) {
+            return res.status(400).json("mat khau khong dung");
+        }
         const token = createToken(user._id);
-
         res.status(200).json({ _id: user._id, name: user.name, email, token });
     } catch (error) {
-
+        console.log(error);
+        res.status(500).json("server error");
+    }
+};
+const findUser = async (req, res) => {
+    const userId = req.params.userId;
+    try {
+        const user = await userModel.findById(userId);
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json("server error");
     }
 };
 
-module.exports = { registerUser, loginUser };
+
+module.exports = { registerUser, loginUser, findUser };
--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser, loginUser } = require("../Controllers/userControllers");
+const { registerUser, loginUser, findUser } = require("../Controllers/userControllers");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/find/:userId", findUser);
 
 module.exports = router;
