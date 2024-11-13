12c699ebdb1445cdfcb196f8111f0405d7847491
Phan Ngọc Thạch <83017432+Entidagreat@users.noreply.github.com>
2024-11-13 15:14:28 +0700

    Foudation up file

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -231,6 +231,28 @@ const ChatBox = () => {
           }}
         />
           </div>
+                 <button
+           classname="send-file"
+           style={{
+            color: '#37342d',
+            backgroundColor: 'transparent',
+            border: 'none',
+            outline: 'none',
+            padding: 0,
+            cursor: 'pointer',
+            fontSize: 'inherit'
+          }}
+          >
+            <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="22" 
+            height="22" 
+            fill="currentColor" 
+            class="bi bi-paperclip" 
+            viewBox="0 0 16 16">
+         <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z"/>
+        </svg>
+          </button>
         <button className="send-btn"
           onClick={() => sendTextMessage(textMessage, user, currentChat._id, setTextMessage)}>
           <svg
